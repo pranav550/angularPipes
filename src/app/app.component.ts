fc0b865c0133311c4d7a5fec563e7aa1411a94e6
pranav <pranavverma550@gmail.com>
2019-04-21 08:05:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPipes';
+  employees:any[]=[
+    {code :'emp001', name:'amit', salary:10000,dob:'02/aug/1991'},
+    {code :'emp002', name:'akash', salary:10000,dob:'02/aug/1992'},
+    {code :'emp003', name:'abhay', salary:10000,dob:'02/aug/1993'},
+    {code :'emp004', name:'ajay', salary:10000,dob:'02/aug/1994'},
+  ]
 }
